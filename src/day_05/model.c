dc5c4f0162a943d82ddb8a38016688786e293426
Andreas Wälchli <andreas.waelchli@me.com>
2025-12-10 19:52:02 +0000

    day 5, part 2

--- src/day_05/model.c
+++ src/day_05/model.c
@@ -3,6 +3,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+static int overlap(RangeNode *node1, RangeNode *node2) {
+    if (node1->start > node2->start) {
+        return overlap(node2, node1);
+    }
+
+    // NODE 1 starts first. only check end index now.
+    if (node1->end >= node2->start - 1) {
+        // node 1 ends after node 2 starts, we have overlap
+        return 1;
+    }
+    return 0;
+}
+
+static void merge(RangeNode *target, RangeNode *additional) {
+    if (additional->start < target->start) {
+        target->start = additional->start;
+    }
+    if (additional->end > target->end) {
+        target->end = additional->end;
+    }
+}
+
 void puzzleAddRange(Puzzle *puzzle, long start, long end) {
     RangeNode *node = malloc(sizeof(RangeNode));
     node->start = start;
@@ -13,9 +35,43 @@ void puzzleAddRange(Puzzle *puzzle, long start, long end) {
         // initialize
         puzzle->rangeHead = node;
         puzzle->rangeTail = node;
+        return;
+    }
+
+    RangeNode *compressionHead = NULL;
+    // insert at the "right" point.
+    if (node->start < puzzle->rangeHead->start) {
+        // add to the front of the list
+        node->next = puzzle->rangeHead;
+        puzzle->rangeHead = node;
+        compressionHead = node;
     } else {
-        puzzle->rangeTail->next = node;
-        puzzle->rangeTail = node;
+        // find right point to insert
+        RangeNode *candidate = puzzle->rangeHead;
+        while (1) {
+            if (candidate->next == NULL || node->start < candidate->next->start) {
+                // we found the right point to insert: between candidate and "next"
+                if (overlap(candidate, node)) {
+                    // in case of overlap, start compression before new node
+                    compressionHead = candidate;
+                } else {
+                    // no overlap with candidate, start compression at new node
+                    compressionHead = node;
+                }
+                node->next = candidate->next;
+                candidate->next = node;
+                break;
+            }
+            candidate = candidate->next;
+        }
+    }
+    // we added a new node. try to compress the now potentially overlapping ranges
+    while (compressionHead->next != NULL && overlap(compressionHead, compressionHead->next)) {
+        RangeNode *droppedNode = compressionHead->next;
+        // there's an overlap, we should merge and drop the merged node
+        merge(compressionHead, droppedNode);
+        compressionHead->next = droppedNode->next;
+        free(droppedNode);
     }
 }
 
@@ -65,4 +121,3 @@ Puzzle *createPuzzle() {
     input->numberTail = NULL;
     return input;
 }
-
--- src/day_05/part2.c
+++ src/day_05/part2.c
@@ -1,6 +1,15 @@
 #include "parts.h"
 
 long part2(Puzzle *input) {
-    // TODO
-    return 0;
+    long counter = 0;
+
+    RangeNode *node = input->rangeHead;
+
+    // range nodes are already sorted, but not yet merged
+    while (node != NULL) {
+        counter += node->end - node->start + 1;
+        node = node->next;
+    }
+
+    return counter;
 }
